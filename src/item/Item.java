203fb8a5cc1abf6e23fa726548aff31a00fd41fe
Tequilamagna <aquilamagna@gmx.at>
2026-04-13 19:36:51 +0200

    implemented fields and methods

--- src/item/Item.java
+++ src/item/Item.java
@@ -2,5 +2,18 @@ package item;
 
 /* TODO: Start your homework implementation here */
 public abstract class Item {
+    public final String name;
 
+    Item(String name) {
+        this.name = name;
+    }
+
+    public abstract double price();
+    public abstract int count();
+    public abstract String prefix();
+
+    @Override
+    public String toString() {
+        return "%s: %s - %.2f Gold".formatted(this.prefix(), this.name, this.price());
+    }
 }
