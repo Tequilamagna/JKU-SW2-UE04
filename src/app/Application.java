2917f1410c9860048f5289f86690a251bb44f2af
Tequilamagna <aquilamagna@gmx.at>
2026-04-13 22:49:51 +0200

    first Idea

--- src/app/Application.java
+++ src/app/Application.java
@@ -2,19 +2,38 @@ package app;
 
 import inout.Out;
 import item.Item;
+import item.Items;
 
 public class Application {
   public static void main(String[] args) {
-    /*
-    TODO: Design a complex item that uses at least one of each item kinds.
-     */
-    Item myItem = null;
+
+    Item uruk = Items.item("Uruk", 1);
+    Item steel = Items.item("Steel", 0.5);
+    Item work = Items.item("work", 0.05);
+    Item sword = Items.item("Sword", steel, work);
+    Item shield = Items.item("Shield", steel, work);
+    Item armour = Items.item("Armour", steel, work);
+    Item fightingUruk = Items.item("Fighting Uruk-Hai", uruk, sword, shield, armour);
+    Item army = Items.item(fightingUruk, 10000); //but my lord, there is no such force
+    Item wizard = Items.item("Wizard", 1000);
+    Item whiteWizard = Items.enchant(wizard, "Level-up", "White");
+    Item saruman = Items.enchant(whiteWizard, "corrupted by Sauron", "fallen");
+    Item pipeWeed = Items.item("Pipe Weed", 10);
+    Item drugDealing = Items.enchant(pipeWeed, "dealt", 600);
+    Item hobbit = Items.item("Wooden Horse", 200);
+    Item suppressedHobbit = Items.enchant(hobbit, "intimidated", "suppressed");
+    Item shireFolk = Items.item(suppressedHobbit,40000);
+    Item men = Items.item("Man of Bree", 10);
+    Item thug = Items.enchant(men, "corrupted by Saruman", "Bad");
+    Item thugs = Items.item(thug, 50);
+
+    Item myItem = Items.item("Army worth of Mordor", saruman, army, drugDealing, thugs, shireFolk);
 
     /* TODO: Call correct functions on myItem */
     Out.println("==========");
-    Out.println("%s consists of %d primitive items".formatted("TODO", -1));
-    Out.println("Its value is %.2f Gold".formatted(-1.0));
-    Out.println("It can be bought for %.2f Gold and sold for %.2f Gold".formatted(-1.0, -1.0));
+    Out.println("%s consists of %d primitive items".formatted(myItem.name, myItem.count()));
+    Out.println("Its value is %.2f Gold.".formatted(myItem.value())); // Probably less than Bilbo's Mithril shirt.
+    Out.println("It can be bought for %.2f Gold and sold for %.2f Gold".formatted(myItem.buyPrice(), myItem.sellPrice()));
     Out.println("==========");
     Out.println(myItem);
   }
